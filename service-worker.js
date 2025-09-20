4784383ccf7466232070ca688d4dae5eb00bf047
Dhidhik Alif Shofianto <didikalief@gmail.com>
2025-09-20 09:42:32 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js') // <-- Mencari file ini
+      .then(registration => {
+        console.log('Service Worker registered: ', registration);
+      })
+      .catch(registrationError => {
+        console.log('Service Worker registration failed: ', registrationError);
+      });
+  });
+}
